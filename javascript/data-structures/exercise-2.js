b24b373ee19bbedce016a1a56f2e864054c03d83
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-05-24 18:46:31 -0400

    finished javascript data structures

--- javascript/data-structures/exercise-2.js
+++ javascript/data-structures/exercise-2.js
@@ -35,17 +35,28 @@ let course = {
 };
 
 // Name of the course ('JavaScript Applications')
-
-// Name of the second teacher ('Shane')
-
-// Name of the first student ('Steve')
-
-// Katy's computer type ('macbook')
-
-// The preReq equipment object
-
-// The second OSOption from equipment prereqs ('osx')
-
+console.log(course.name);
+// // Name of the second teacher ('Shane')
+console.log(course.teachers[1]);
+// // Name of the first student ('Steve')
+console.log(course.students[0].name);
+// // Katy's computer type ('macbook')
+console.log(course.students[1].computer.type);
+// // The preReq equipment object
+console.log(course.preReqs);
+// // The second OSOption from equipment prereqs ('osx')
+console.log(course.preReqs.equipment.OSOptions[1]);
 // String listing the OSOptions separated by 'or' ('linux or osx')
-
+console.log(course.preReqs.equipment.OSOptions.join(" or "));
 // An array of all the students that are using osx
+const studentsUsingOSX = [];
+
+for (let i = 0; i < course.students.length; i++) {
+  console.log(course.students[i].computer.OS);
+  if (course.students[i].computer.OS === "OSX") {
+    console.log("You're using OSX");
+    studentsUsingOSX.push(course.students[i]);
+  }
+}
+
+console.log(studentsUsingOSX);
---
+++ javascript/data-structures/valueReference.js
@@ -0,0 +1,19 @@
+const numOne = 1;
+const numTwo = 1;
+
+// Comparison of numbers looks at the value.
+// console.log(numOne === numTwo);
+
+const classOne = {
+  name: "Intro to JavaScript",
+};
+
+const classTwo = {
+  name: "Intro to JavaScript",
+};
+
+// Comparison of objects it looks at the reference.
+console.log(classOne === classTwo);
+// console.log(JSON.stringify(classOne) === JSON.stringify(classTwo));
+
+// console.log(JSON.stringify(classOne));
