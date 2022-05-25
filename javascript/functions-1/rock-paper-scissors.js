47000cc7fb8063804408be780acee1ac79cedee2
Jonathan J. Watson <jonathanwatson1@gmail.com>
2022-05-24 21:03:08 -0400

    logic

--- javascript/functions-1/rock-paper-scissors.js
+++ javascript/functions-1/rock-paper-scissors.js
@@ -2,7 +2,7 @@
 
 // Define a hands array with the values 'rock', 'paper', and 'scissors'
 const hands = ["rock", "paper", "scissors"];
-console.log(hands[0]);
+// console.log(hands[0]);
 // Define a function getComputerHand() that returns a hand from the array using parseInt(Math.random()*10) % 3)
 
 // console.log(randomNumber);
@@ -12,10 +12,26 @@ function getComputerHand() {
   return computerHand;
 }
 
-console.log(getComputerHand());
 // Define a function called compare() that
+function compare(computerChoice, personChoice) {
+  if (computerChoice === "rock" && personChoice === "paper") {
+    return "You win!";
+  } else if (computerChoice === "rock" && personChoice === "scissors") {
+    return "You lose!";
+  } else {
+    return "Tie game!";
+  }
+}
 // Takes two choices as arguments
 // Determines the winner
 // Logs the hands played by each
 // Returns a string indicating the result
 // Play a game
+
+const computerHand = getComputerHand();
+const playerHand = hands[parseInt(Math.random() * 10) % 3];
+console.log(computerHand);
+console.log(playerHand);
+
+const result = compare(computerHand, playerHand);
+console.log(result);
